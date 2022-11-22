a4c0eaf8d0f32b5415c077d3467e9abc43a10766
Raphael Assis <58819530+AssisRaphael@users.noreply.github.com>
2022-11-22 11:59:46 -0300

    feat: add db service

--- src/Services/dataBaseService.java
+++ src/Services/dataBaseService.java
@@ -1,8 +1,22 @@
 package Services;
-import com.opencsv.CSVWriter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class dataBaseService {
 	private String dbName;
+	private FileWriter dbWriter;
+	private BufferedReader dbReader;
+
+	public dataBaseService(String dbName) throws IOException {
+		File dbFile = new File(dbName);
+		this.dbWriter = new FileWriter(dbFile);
+		this.dbReader = new BufferedReader(new FileReader(dbFile));
+	}
 
 	public String getDbName() {
 		return dbName;
@@ -11,7 +25,34 @@ public class dataBaseService {
 	public void setDbName(String dbName) {
 		this.dbName = dbName;
 	}
-	
-	
-	
+
+	public void writeLine(String newDbLine) throws IOException {
+		dbWriter.write(newDbLine);
+		dbWriter.flush();
+	}
+
+	public ArrayList<String> ReadLines() throws IOException {
+		String lineRead;
+		ArrayList<String> linesRead = new ArrayList<String>();
+		while ((lineRead = dbReader.readLine()) != null) {
+			linesRead.add(lineRead);
+		}
+		return linesRead;
+	}
+
+	public String findLine(String match) throws IOException {
+		String lineRead;
+		while ((lineRead = dbReader.readLine()) != null) {
+			if (lineRead.contains(match)) {
+				return lineRead;
+			}
+		}
+		return null;
+	}
+
+	public void closeConnection() throws IOException {
+		dbReader.close();
+		dbWriter.close();
+	}
+
 }
