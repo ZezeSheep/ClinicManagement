4d2bc8ad18159273df554bd2763daa750ea68cef
Raphael Assis <58819530+AssisRaphael@users.noreply.github.com>
2022-11-22 10:28:44 -0300

    crwate service

---
+++ src/Services/dataBaseService.java
@@ -0,0 +1,17 @@
+package Services;
+import com.opencsv.CSVWriter;
+
+public class dataBaseService {
+	private String dbName;
+
+	public String getDbName() {
+		return dbName;
+	}
+
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+	
+	
+	
+}
