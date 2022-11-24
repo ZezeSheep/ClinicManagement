28b22919e39a9a5de359abeea79aae3b07403de3
Jose Antonio <jose.avila@pdcase.com.br>
2022-11-24 08:03:18 -0300

    tela do cliente feita

--- src/controller/ViewController.java
+++ src/controller/ViewController.java
@@ -1,5 +1,24 @@
 package controller;
 
+import screen.ClientScreen;
+
 public class ViewController {
+	
+	private ClientScreen clientScreen;
+	
+	public ViewController() {
+		clientScreen = new ClientScreen(this);
+	}
+
+	public ClientScreen getClientScreen() {
+		return clientScreen;
+	}
+
+	public void setClientScreen(ClientScreen clientScreen) {
+		this.clientScreen = clientScreen;
+	}
+	
+	
+	
 
 }
--- src/screen/Screen.java
+++ src/screen/Screen.java
@@ -1,5 +1,31 @@
 package screen;
 
+import controller.ViewController;
+
 public abstract class Screen {
+	
+	private ViewController viewController;
+	
+	public Screen(ViewController viewController) {
+		this.viewController = viewController;
+	}
+	
+	protected abstract void show();
+	protected abstract void changeScreen();
+	
+	public void execute() {
+		show();
+		changeScreen();
+	}
+
+	public ViewController getViewController() {
+		return viewController;
+	}
+
+	public void setViewController(ViewController viewController) {
+		this.viewController = viewController;
+	}
+	
+	
 
 }
